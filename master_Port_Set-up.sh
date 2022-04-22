d61a990f57c1bf3139e4ddd11e11f7c21bb21713
pashadecoded <pasha.decoded@gmail.com>
2022-04-23 02:47:39 +0530

    added weave.yml CNI

--- master_Port_Set-up.sh
+++ master_Port_Set-up.sh
@@ -1,7 +1,7 @@
 # Iptable rules for opening ports in a linux machine
 
 sudo iptables -A INPUT -p tcp --dport 6443 -j ACCEPT
-Sudo iptables -A INPUT -p tcp --match multiport --dports 2379:2380 -j ACCEPT
+sudo iptables -A INPUT -p tcp --match multiport --dports 2379:2380 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 10250 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 10259 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 10257 -j ACCEPT
--- worker_Port_Set-up.sh
+++ worker_Port_Set-up.sh
@@ -1,4 +1,4 @@
 # Worker node iptable rules for opening port
 
 sudo iptables -A INPUT -p tcp --dport 10250 -j ACCEPT
-Sudo iptables -A INPUT -p tcp --match multiport --dports 30000:32767 -j ACCEPT
+sudo iptables -A INPUT -p tcp --match multiport --dports 30000:32767 -j ACCEPT
